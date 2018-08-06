02f08c80a977c478ed74ce4e267a52c655d07e9a
Yaroslav Reshetnik <yar_resh@i.ua>
2018-08-06 15:54:24 +0300

    Made builder required for prepare_send function.

--- app.py
+++ app.py
@@ -15,10 +15,9 @@ def gen_builder(secret_key, network):
     return builder
 
 
-def prepare_send(from_secret, to_pubkey, amount, asset_code, network='TESTNET', builder=None):
+def prepare_send(builder, from_secret, to_pubkey, amount, asset_code):
     """Prepare asset transfer."""
     from_pubkey = stellar_base.keypair.Keypair.from_seed(from_secret).address()
-    builder = builder or gen_builder(from_secret, network)
     builder.append_payment_op(to_pubkey, amount, asset_code, from_pubkey)
     return builder.gen_te().xdr().decode()
 
@@ -37,7 +36,7 @@ def main():
     asset_code = sys.argv[2]
     recipient_pubkey = sys.argv[3]
     builder = gen_builder(secret_key, 'TESTNET')
-    prepare_send(secret_key, recipient_pubkey, '10.0', asset_code, builder=builder)
+    prepare_send(builder, secret_key, recipient_pubkey, '10.0', asset_code)
     try:
         submit(builder)
     except StellarTransactionFailed as exc:
